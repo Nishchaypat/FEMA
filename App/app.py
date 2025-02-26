23b430622f68c320afca325af0a1869dcfbf122c
Nishchay Patel <100232114+Nishchaypat@users.noreply.github.com>
2025-02-25 21:03:37 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -12,6 +12,7 @@ import pickle
 from flask_cors import CORS
 
 #Applying the Pipeline
+
 class FastBertPredictor:
     def __init__(self, model_path=r'/Users/npatel237/Library/CloudStorage/OneDrive-GeorgiaStateUniversity/FEMA/Best_82'):
         print("Loading BERT model...")
